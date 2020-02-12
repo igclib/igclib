b1afd78fa58d82247c4916334c67ab9941307e57
Téo Bouvard <teobouvard@gmail.com>
2020-02-12 08:14:24 +0100

    Change file directory to data
    All tests can reference input files located inside this directory.

---
+++ tests/test_xc.cpp
@@ -0,0 +1,13 @@
+#include "igclib/flight.hpp"
+#include "assert.h"
+#include <iostream>
+
+int main(int argc, char *argv[]){
+  Flight f("data/flights/xc_col_agnel.igc");
+  if (f.pilot_name != "Téo Bouvard"){
+    std::cerr << "Test failed" << std::endl;
+  }
+
+  std::cerr << "Test passed" << std::endl;
+  return EXIT_SUCCESS;
+}
