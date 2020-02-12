1c727fb0c8fc2ff711207ba292043d7e45caece8
Téo Bouvard <teobouvard@gmail.com>
2020-02-12 08:11:48 +0100

    Add test data to test binary tree
    Using COPY directive of CMake allows to reference test data files with a
    relative path inside the tests.

--- include/igclib/flight.hpp
+++ include/igclib/flight.hpp
@@ -11,11 +11,12 @@ public:
   Flight(const std::string &filename);
   void to_JSON() const;
 
+  std::string pilot_name = "Unknown pilot";
+  
 private:
   void process_H_record(const std::string &record);
   void process_B_record(const std::string &record);
 
   std::map<Time, GeoPoint> points;
-  std::string pilot_name = "Unknown pilot";
   int time_zone_offset = 0;
 };
--- tests/xc_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "igclib/flight.hpp"
-#include "assert.h"
-
-int main(int argc, char *argv[]){
-  Flight f("flights/xc_col_agnel.igc");
-  assert (f.pilot_name == "Téo Bouvard");
-
-  return EXIT_SUCCESS;
-}
